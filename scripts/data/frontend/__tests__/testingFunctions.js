32dc819d0f4b01a268a66dd4037ef83c23919b13
Elizabeth Lotto <elizlotto@gmail.com>
2020-08-19 20:52:20 -0400

    beginning test with jest

---
+++ scripts/data/frontend/__tests__/testingFunctions.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+import webpack from './webpack.js';
+import script from './script.js';
+
+describe('Webpackjs', () => {
+    it('should prompt the user with questions', () => {
+        const callback = jest.fn();
+        callScriptjs(callback); //do we have a function we can invoke script.js with?
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
+    it('should generate a webpack.config.js file', () => {
+        expect(subject(/* look into how to test a new file creation? /*/).toBe('/Week6/create-fullstack-webpack-app/webpack.config.js')
+    })
+})
